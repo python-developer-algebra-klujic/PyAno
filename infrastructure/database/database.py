ee7e8b1fb8f0841c24da24ec93d41a4cc9929186
Python Developer Algebra Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-11-13 18:38:35 +0100

    Update database.py

--- infrastructure/database/database.py
+++ infrastructure/database/database.py
@@ -5,6 +5,9 @@ from config import DATABASE_PATH
 
 
 engine = create_engine(DATABASE_PATH)
+
+# Koristiti cemo u MODEL klasama
 Base = declarative_base()
 
+# Koristiti cemo u REPO klasama
 Session = sessionmaker(bind=engine)
